6d44e7defa90d05572ec44c001791b2dffe643c5
Felix Tuffreaud <felix.tuffreaud@laposte.net>
2018-11-28 22:59:33 +0100

    add form with symfony4

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,6 +40,27 @@ class BlogController extends AbstractController
     }
 
     /**
+     * @Route("/blog/new", name="blog_create")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request, ObjectManager $manager)
+    {
+
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                    ->add('title')
+                    ->add('content')
+                    ->add('image')
+                    ->getForm();
+
+        return $this->render('blog/create.html.twig', ['formArticle' => $form->createView()]);
+    }
+
+
+    /**
      * @Route("/blog/{id}", name="blog_show")
      * @param Article $article
      * @return \Symfony\Component\HttpFoundation\Response
@@ -43,4 +69,7 @@ class BlogController extends AbstractController
     {
         return $this->render('blog/show.html.twig', ['article' => $article]);
     }
+
+
 }
+
